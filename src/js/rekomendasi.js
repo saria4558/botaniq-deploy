f2d70dc20d2fb1db563639c56ab3350905b054f3
saria4558 <saria4558@gmail.com>
2025-06-12 16:07:49 +0700

    finish project

--- src/js/rekomendasi.js
+++ src/js/rekomendasi.js
@@ -518,3 +518,20 @@ document.querySelector('.back-link').addEventListener('click', function (e) {
     history.back(); // atau history.go(-1);
 });
 });
+
+  document.getElementById('logoutbtn').addEventListener('click', function (e) {
+    e.preventDefault();
+    localStorage.removeItem('token'); // ganti 'token' sesuai nama penyimpananmu
+    sessionStorage.removeItem('token');
+
+    fetch(`${BASE_API_URL}/logout`, {  
+        method: 'POST'
+    }).then(res => res.json()).then(data => {
+        console.log(data.message); 
+       
+        window.location.href = 'home.html'; 
+    }).catch(err => {
+        console.error('Logout error:', err);
+        window.location.href = 'home.html'
+    });
+});
